cd3f2d7399f7ca25b4d015356d0b6c51167a7ae5
mglgnzlz <mglgnzlz13@gmail.com>
2024-08-08 17:38:54 +0800

    [FEATURE] Added SSH Credential loading via json in APPDATA, modularized script loading

--- dobaMainFile/main.py
+++ dobaMainFile/main.py
@@ -1,10 +1,20 @@
-from dbGUI import App 
 import tkinter as tk
+from dbGUI import App
+from dbMonitor import UploadLogic
+import json
+import os
 
-def main():
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
-    
+def load_ssh_credentials():
+    path = os.path.join(os.getenv('APPDATA', os.path.expanduser('~')), "ssh_credentials.json")
+    if os.path.exists(path):
+        with open(path, "r") as file:
+            return json.load(file)
+    return {"host": "", "port": "", "username": "", "password": ""}
+
 if __name__ == "__main__":
-    main()
+    ssh_credentials = load_ssh_credentials()
+    upload_logic = UploadLogic(ssh_credentials)
+    
+    root = tk.Tk()
+    app = App(root, upload_logic)
+    root.mainloop()
